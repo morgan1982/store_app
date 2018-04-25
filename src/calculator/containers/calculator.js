12e9a279bfb6cc890e9d024ac967012cdf6f7457
entropy <alexandrisanas@gmail.com>
2018-04-26 00:56:24 +0300

    checkbox

--- src/calculator/containers/calculator.js
+++ src/calculator/containers/calculator.js
@@ -11,9 +11,11 @@ export default class SwitchExample extends Component {
         super(props);
 
         this.state = {
-            checked: false,
+            nr_checked: false,
+            lc_checked: true,
             total: "1€",
-            input_val: 0
+            input_val: 0,
+            ticked: true
         }
 
         this.handleChange = this.handleChange.bind(this);
@@ -22,55 +24,55 @@ export default class SwitchExample extends Component {
     }
 
     handleChange () {
-        let checked = false;
-        this.state.checked ? checked = false : checked = true;
-        this.setState({ checked });
+        let nr_checked = false;
+        this.state.nr_checked ? nr_checked = false : nr_checked = true;
+        this.setState({ nr_checked });
 
     }
     handleInput (e) {
+        let price = e.target.value;
         this.setState({
-            input_val: e.target.value
+            input_val: price
         })
-        this.totalPrice();
+        // this.totalPrice();      
     }
-    totalPrice () {
-        let price;
-        let init_price = this.state.input_val
-        // console.log(price);
-        this.state.checked ? price = init_price * 1.0 / 1.24 : price = init_price;
-        Math.round(price, 2);
-        console.log("the price is", price);
+    totalPrice (val) {
+        let total = this.state.nr_checked ? parseFloat(val) / 1.24 : val;
+        total = this.state.lc_checked ? total / 1.1 : total;
+        total = Math.round(total * 100) / 100;
+        return total; 
+    }
+    checkboxChange = (e) => {
+        let tick = false;
+        this.state.ticked ? tick = false : tick = true;
         this.setState({
-            total: price
+            ticked: tick
         })
+        console.log(e.target.checked)
     }
-    testing = () => {
-        const total = this.state.total;
-        return <h1>{total}</h1>
-        console.log("changed")
-    } 
+
+
 
 
 
 
     render() {
+        const price = this.totalPrice(this.state.input_val);
 
-            const toggle = this.state.checked;
+            const toggle = this.state.nr_checked;
             const indicator = toggle ? (
                 <h1 style={{"color": "green"}}>ON</h1>
             ) : (
                 <h1 style={{"color": "red"}}>OFF</h1>
             )
-            const test = this.testing();
 
 
         return (
             <div>
             <label htmlFor="normal-switch">
-                <span>Switch with default style</span>
                 <Switch 
                     onChange={this.handleChange}
-                    checked={this.state.checked}
+                    checked={this.state.nr_checked}
                     id="normal-switch"
                     boxShadow="1px 1px 5px black"
                     handleDiameter={22}
@@ -86,8 +88,10 @@ export default class SwitchExample extends Component {
                        onChange={this.handleInput}
                        value={this.state.input_val}/>
                 <h2><strong>Total</strong></h2>
-                <h2>{this.state.total}</h2>
-                {test}
+                <h3>{price}</h3>
+                <input type="checkbox"
+                       checked={this.state.ticked}
+                       onChange={this.checkboxChange}/>
             </div>
 
         );
